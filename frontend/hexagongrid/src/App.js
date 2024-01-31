04dd7c51c57186779da121c8b3a55c793a419f22
tteenntt <87744652+Unlxii@users.noreply.github.com>
2024-01-31 14:46:31 +0700

    added prototpye hexagongrid
    added prototype but can't fix column and rows when want to put to rows and column only can change hight and width

---
+++ frontend/hexagongrid/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import HexGridDemo from "./Grid.js";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HexGridDemo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
