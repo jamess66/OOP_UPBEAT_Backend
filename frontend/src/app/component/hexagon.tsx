5eadfdbc544955a1a555c7b0a494d00c8f952af3
tteenntt <87744652+Unlxii@users.noreply.github.com>
2024-02-26 18:46:21 +0700

    change hexagon split component

--- frontend/src/app/component/hexagon.tsx
+++ frontend/src/app/component/hexagon.tsx
@@ -19,6 +19,7 @@ interface HexGrid {
   row: number;
   col: number;
 }
+
 async function fetchUsers(): Promise<HexGrid[]> {
   const response = await fetch("http://localhost:8080/territory");
   const data = await response.json();
@@ -31,6 +32,50 @@ function CreateHexagon() {
     fetchUsers().then((data) => setHexagon(data));
   }, []);
 }
+function ProfileBox({
+  isVisible,
+  handleClick,
+}: {
+  isVisible: boolean;
+  handleClick: () => void;
+}) {
+  return (
+    <div className="box">
+      {!isVisible && (
+        <button
+          className={`Profile ${isVisible ? "hidden" : ""}`}
+          onClick={handleClick}
+        >
+          Profile
+        </button>
+      )}
+      {isVisible && (
+        <div
+          className={`Profilehidden ${isVisible ? "" : "hidden"}`}
+          onClick={handleClick}
+        >
+          Player1 :
+          <br />
+          Coin :
+        </div>
+      )}
+      <div className="box_cover"></div>
+    </div>
+  );
+}
+
+function InputForm() {
+  return (
+    <form className="input">
+      <input
+        type="input"
+        placeholder=" / Write command here"
+        className="input_box"
+      ></input>
+    </form>
+  );
+}
+
 function Hexagon() {
   const column = 15;
   const row = 20;
@@ -54,77 +99,79 @@ function Hexagon() {
   };
 
   return (
-    // <div>
-    //   <button onClick={zoomIn} className=" bg-white">
-    //     Zoom In
-    //   </button>
-    //   <button onClick={zoomOut}>Zoom Out</button>
-
     <div
       style={{
-        marginTop: "45px",
-        marginLeft: "45%",
-        // transform: `scale(${zoom})`,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "flex-start",
       }}
     >
-      {arr2.map((data, key) => {
-        return (
-          <div
-            style={{
-              display: "flex",
-              marginLeft: "25%",
-            }}
-            key={key}
-          >
-            {data.map((data2, k) => {
-              return (
-                <div
-                  key={k}
-                  style={{
-                    transform: `translate(-${(35 * k) / 4}px, ${
-                      k % 2 === 0 ? "21px" : "0px"
-                    })`,
-                    marginTop: k % 2 === 0 ? "3px" : "0px",
-                  }}
-                  className={"hex-grid-content"}
-                ></div>
-              );
-            })}
-          </div>
-        );
-      })}
-      <div className="Round"> ROUND :</div>
-      <div className="Time">TIME :</div>
-      <div className="box">
-        {!isVisible && (
-          <button
-            className={`Profile ${isVisible ? "hidden" : ""}`}
-            onClick={handleClick}
-          >
-            Profile
-          </button>
-        )}
-        {isVisible && (
-          <div
-            className={`Profilehidden ${isVisible ? "" : "hidden"}`}
-            onClick={handleClick}
-          >
-            Player1 :
-            <br />
-            Coin :
-          </div>
-        )}
-        <div className="box_cover"></div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginLeft: "12%",
+          marginTop: "26%",
+          flexShrink: 0,
+        }}
+      >
+        <div className="Round"> ROUND :</div>
+        <div className="Time">TIME :</div>
+        <ProfileBox isVisible={isVisible} handleClick={handleClick} />
+        <InputForm />
       </div>
-      <form className="input">
-        <input
-          type="input"
-          placeholder=" / Write command here"
-          className="input_box"
-        ></input>
-      </form>
+      <div
+        style={{
+          marginTop: "45px",
+          marginLeft: "20%",
+          flexDirection: "row-reverse",
+          justifyContent: "flex-end",
+        }}
+      >
+        {arr2.map((data, y) => {
+          return (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+              }}
+              key={y}
+            >
+              {data.map((data2, x) => {
+                return (
+                  <div
+                    key={x}
+                    style={{
+                      transform: `translate(-${(35 * x) / 4}px, ${
+                        x % 2 === 0 ? "21px" : "0px"
+                      })`,
+                      marginTop: x % 2 === 0 ? "3px" : "0px",
+                    }}
+                    className={"hex-grid-content"}
+                  >
+                    <div
+                      style={{
+                        position: "relative",
+                        color: "black",
+                        fontSize: "10px",
+                        alignItems: "center",
+                        transform: `translate(-50%, -50%)`,
+                        top: "50%",
+                        left: "50%",
+                      }}
+                    >
+                      {`(${x}, ${y})`}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
-    // </div>
   );
 }
 
